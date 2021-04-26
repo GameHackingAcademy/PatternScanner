a1b89a6798b9707343d85effd2d5e621889f7937
attilathedud <attilathedud@gmail.com>
2021-04-26 11:49:50 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <tlhelp32.h>
 #include <stdio.h>
 
+// Our opcode pattern to scan for inside the process
 unsigned char bytes[] = { 0x29, 0x42, 0x04 };
 
 int main(int argc, char** argv) {
@@ -21,23 +22,33 @@ int main(int argc, char** argv) {
 	Process32First(process_snapshot, &pe32);
 
 	do {
+		// Only scan for patterns inside the Wesnoth process
 		if (wcscmp(pe32.szExeFile, L"wesnoth.exe") == 0) {
 			module_snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, pe32.th32ProcessID);
+			
+			// Retrieve a process handle so that we can read the game's memory
 			HANDLE process = OpenProcess(PROCESS_ALL_ACCESS, true, pe32.th32ProcessID);
 
 			Module32First(module_snapshot, &me32);
 			do {
+				// Wesnoth is made up of many modules. For our example, we only want to scan the main executable module's code
 				if (wcscmp(me32.szModule, L"wesnoth.exe") == 0) {
+					// Due to the size of the code, dynamically create a buffer after determining the size
 					unsigned char *buffer = (unsigned char*)calloc(1, me32.modBaseSize);
 					DWORD bytes_read = 0;
 
+					// Read the entire code block into our buffer
 					ReadProcessMemory(process, (void*)me32.modBaseAddr, buffer, me32.modBaseSize, &bytes_read);
+					
+					// For each byte in the game's code, check to see if the pattern of bytes starts at the byte
 					for (unsigned int i = 0; i < me32.modBaseSize - sizeof(bytes); i++) {
 						for (int j = 0; j < sizeof(bytes); j++) {
+							// If so, continue to check if all the bytes match. If one does not, exit the loop
 							if (bytes[j] != buffer[i + j]) {
 								break;
 							}
 
+							// If we are at the end of the loop, the bytes must all match
 							if (j + 1 == sizeof(bytes)) {
 								printf("%x\n", i + (DWORD)me32.modBaseAddr);
 							}
