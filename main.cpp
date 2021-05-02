6c64f041e521fe72776661ce122ccae5fa67c251
attilathedud <attilathedud@gmail.com>
2021-05-02 10:26:58 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+/*
+A pattern scanner that will search a running Wesnoth process for the bytes 0x29 42 04. These bytes are the opcode for the sub instruction 
+that is responsible for subtracting gold from a player when recruiting a unit. 
+
+The scanner works by using CreateToolhelp32Snapshot to find the Wesnoth process and the main Wesnoth module. 
+Once located, a buffer is created and the module's memory is read into that buffer. The module's memory mainly contains opcodes for instruction.
+
+Once loaded, we loop through all the bytes in the buffer and search for our pattern. Once found, we print the offset.
+*/
+
 #include <windows.h>
 #include <tlhelp32.h>
 #include <stdio.h>
